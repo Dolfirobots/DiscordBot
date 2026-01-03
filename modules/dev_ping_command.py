320138c87d450467ab2de1891f53ef12aa8ced4a
Dolfirobots <dolfirobots@gmail.com>
2026-01-03 18:27:30 +0100

    feat(ping): add ping command to measure bot response time

---
+++ modules/dev_ping_command.py
@@ -0,0 +1,13 @@
+import disnake
+from disnake.ext import commands
+
+class PingModule(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.slash_command(description="Test response time")
+    async def ping(self, inter: disnake.ApplicationCommandInteraction):
+        await inter.response.send_message(f"It took {round(self.bot.latency * 1000)}ms to respond!")
+
+def setup(bot):
+    bot.add_cog(PingModule(bot))
