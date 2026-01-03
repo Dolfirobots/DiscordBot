516b9567f46b32d51df19393f9eac986f2d6bb69
Dolfirobots <dolfirobots@gmail.com>
2026-01-03 14:19:02 +0100

    feat(config): implement configuration management with file handling

---
+++ config.py
@@ -0,0 +1,44 @@
+import pathlib
+import aiofiles
+
+from logger import logger
+
+class FileType:
+    TXT = "txt"
+    JSON = "json" # in work
+    YAML = "yaml" # in work
+
+class Config:
+    def __init__(self, relative_path: str, file_type: FileType):
+        self.root = pathlib.Path(__file__).parent.resolve()
+        self.path = self.root / "config" / relative_path
+        self.file_type = file_type
+    
+    # Checking if config file exists, if not create it
+    async def validate(self, default_content: str = ""):
+        if not self.path.exists():
+            self.path.parent.mkdir(parents=True, exist_ok=True)
+            
+            logger.warning(f"Config file {self.path} does not exist. Creating a new one.")
+            try:
+                async with aiofiles.open(self.path, mode='w', encoding='utf-8') as f:
+                    await f.write(default_content)
+                logger.info(f"Created new config file at {self.path}.")
+            except Exception as e:
+                logger.error(f"Error creating config file {self.path}: {e}")
+
+    # TXT methods
+    async def get_lines(self, with_comments: bool = False) -> list[str]:
+        try:
+            async with aiofiles.open(self.path, mode='r', encoding='utf-8') as f:
+                content = await f.read()
+                lines = [
+                    line.strip() 
+                    for line in content.splitlines() 
+                    if line.strip() and (not with_comments and not line.strip().startswith("#"))
+                ]
+            return lines
+        except Exception as e:
+            logger.error(f"Error reading config file {self.path}: {e}")
+            return []
+        
