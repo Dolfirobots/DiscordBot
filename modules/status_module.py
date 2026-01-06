966a5247c839ad36c7658902cef779aa180c1f45
Dolfirobots <dolfirobots@gmail.com>
2026-01-06 13:23:17 +0100

    feat(status): release the finale module

--- modules/status_module.py
+++ modules/status_module.py
@@ -10,7 +10,7 @@ from minecraft.status import get_server_status
 class StatusModule(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot: commands.Bot = bot
-        self.mc_ip = "mc.hypixel.net"
+        self.mc_ip = "Dolfirobots.net"
         self.mc_port = 25565
         self.update_mc_status.start()
 
@@ -20,31 +20,22 @@ class StatusModule(commands.Cog):
     @tasks.loop(seconds=10)
     async def update_mc_status(self):
         status = await get_server_status(self.mc_ip, self.mc_port, timeout=5)
-
+        
         if status:
             online = status.get("players_online", 0)
-            max_p = status.get("players_max", 0)
-            motd = status.get("plain_description", "Minecraft Server")
-            version = status.get("version_name", "1.20.x")
+            max = status.get("players_max", 0)
+            activity = disnake.Activity(
+                type=disnake.ActivityType.watching, 
+                name=f"{self.mc_ip}: {online}/{max}"
+            )
 
-        activity = disnake.Activity(
-            application_id=25565,
-            name="Test",
-            type=disnake.ActivityType.playing,
-            state="State",
-            details="Details",
-            assets={
-                "large_image": "server_icon",
-                "small_image": "online_dot",
-            },
-            party={
-                "id": 1222,
-                "size": (1, 5)
-            }
-        )
-
+        else:
+            activity = disnake.Activity(
+                type=disnake.ActivityType.competing, 
+                name="🔴 Offline"
+            )
+        
         await self.bot.change_presence(activity=activity)
-
     @update_mc_status.before_loop
     async def before_update(self):
         await self.bot.wait_until_ready()
