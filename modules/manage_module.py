17c8367fef2c81f92ba1603599819bcb7686a9ff
Dolfirobots <dolfirobots@gmail.com>
2026-01-25 19:12:33 +0100

    perf(ticket manager): remove category check that needs to be loaded from the config

--- modules/manage_module.py
+++ modules/manage_module.py
@@ -29,12 +29,6 @@ class ManageView(disnake.ui.View):
         ns_time = time.time_ns()
         await inter.response.defer()
 
-        data = await MAIN_CONFIG.load_json()
-        tickets_category_id = data.get("ticket", {}).get("ticket_category")
-
-        if inter.channel.category_id != tickets_category_id:
-            return
-
         ticket = await fetch_ticket_by_channel(inter.bot, inter.channel_id)
         if not ticket:
             return await inter.followup.send(
